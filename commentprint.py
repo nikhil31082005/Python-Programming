12c87b413cae9655b74d57cfa085114169973c58
nikhil31082005 <122168803+nikhil31082005@users.noreply.github.com>
2023-01-31 17:44:50 +0530

    Add files via upload

---
+++ commentprint.py
@@ -0,0 +1,16 @@
+# This is a single line comment
+"""
+This is a
+multiline
+comment
+"""
+# print("Nikhil",end=" ")
+# print("Raj Pratap Singh")
+# print("Nikhil","Raj Pratap Singh")
+#print("Nikhil\\nRaj Pratap Singh")
+# Escape Sequence
+"""
+\n = New line
+\t = Tab space
+
+"""
