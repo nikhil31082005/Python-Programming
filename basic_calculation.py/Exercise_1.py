0f0949905242db4d4fa3c122d96e49a8ed3179c8
nikhil31082005 <122168803+nikhil31082005@users.noreply.github.com>
2023-02-25 06:48:52 +0530

    Add files via upload

---
+++ basic_calculation.py/Exercise_1.py
@@ -0,0 +1,9 @@
+"""
+print on console
+First program - Python print function
+It is declare like this .
+print('What is print')
+"""
+print("First program - Python print function")
+print("It is declare like this .")
+print("print('What is print')")
