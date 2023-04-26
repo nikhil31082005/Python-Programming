8b5fdffe8287df22cddf285ca7ac42bb72b947c8
Nikhil <122168803+nikhil31082005@users.noreply.github.com>
2023-04-26 20:34:27 +0530

    Update rock_paper_scissor_game.py

--- rock_paper_scissor_game.py
+++ rock_paper_scissor_game.py
@@ -9,7 +9,7 @@ while True:
         choices = ['rock','paper','scissor']
         computer_choice = random.choice(choices)
         user_choice = input('Enter your choice : ')
-        user_choice.lower()
+        user_choice = user_choice.lower()
 
         if user_choice == 'rock' and computer_choice == 'scissor' or user_choice == 'paper' and \
             computer_choice == 'rock' or user_choice == 'scissor' and computer_choice == 'paper':
@@ -30,7 +30,7 @@ while True:
         print('DO YOU WANT TO PLAY THIS GAME ONE MORE TIME.')
         print("IF YES :ENTER 'YES' OTHERWISE 'NO")
         response = input()
-        response.lower()
+        response = response.lower()
         if response == 'yes':
             continue
         else:
@@ -39,8 +39,8 @@ while True:
     else:
         user1_choice = input('Enter user 1 choice : ')
         user2_choice = input('Enter user 2 choice : ')
-        user1_choice.lower()
-        user2_choice.lower()
+        user1_choice = user1_choice.lower()
+        user2_choice = user2_choice.lower()
 
         if user1_choice == 'rock' and user2_choice == 'scissor' or user1_choice == 'paper' and \
             user2_choice == 'rock' or user1_choice == 'scissor' and user2_choice == 'paper':
@@ -61,7 +61,7 @@ while True:
         print('DO YOU WANT TO PLAY THIS GAME ONE MORE TIME.')
         print("IF YES :ENTER 'YES' OTHERWISE 'NO")
         response = input()
-        response.lower()
+        response = response.lower()
         if response == 'yes':
             continue
         else:
